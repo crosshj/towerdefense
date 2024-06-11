6ced11897d8dc3ef9c29bffcffd41e4f951db488
crosshj <github@chimpjuice.com>
2024-06-11 11:21:07 -0400

    Create getViewportDimensions.js

---
+++ utils/getViewportDimensions.js
@@ -0,0 +1,25 @@
+function getViewportDimensions() {
+  // Create a temporary element to measure height
+  const tempHeightElement = document.createElement('div');
+  tempHeightElement.style.height = '100vh';
+  tempHeightElement.style.width = '100vw';
+  tempHeightElement.style.position = 'absolute';
+  tempHeightElement.style.top = '0';
+  tempHeightElement.style.left = '0';
+  tempHeightElement.style.visibility = 'hidden';
+  tempHeightElement.style.pointerEvents = 'none';
+
+  // Append it to the body
+  document.body.appendChild(tempHeightElement);
+
+  // Get the dimensions in pixels
+  const height = tempHeightElement.clientHeight;
+  const width = tempHeightElement.clientWidth;
+
+  // Remove the temporary element
+  document.body.removeChild(tempHeightElement);
+
+  return { height, width };
+}
+
+module.exports.getViewportDimensions = getViewportDimensions;
