d3ba4047dd633396a3364e7691d0048c455de1a3
crosshj <github@chimpjuice.com>
2024-06-12 00:23:48 -0400

    troubleshoot tiny scrollbar issue

--- utils/getViewportDimensions.js
+++ utils/getViewportDimensions.js
@@ -15,28 +15,28 @@ const domLoaded = async () => {
 export const getViewportDimensions = async () => {
 	await domLoaded();
 
-	document.body.style.height = '100vh';
-	document.body.style.width = '100vw';
-	const tempHeightElement = document.createElement('div');
-	tempHeightElement.style.height = '100vh';
-	tempHeightElement.style.width = '100vw';
-	tempHeightElement.style.position = 'absolute';
-	tempHeightElement.style.top = '0';
-	tempHeightElement.style.left = '0';
-	tempHeightElement.style.visibility = 'hidden';
-	tempHeightElement.style.pointerEvents = 'none';
-	document.body.appendChild(tempHeightElement);
-	const height = tempHeightElement.clientHeight;
-	const width = tempHeightElement.clientWidth;
-	document.body.removeChild(tempHeightElement);
+	// document.body.style.height = '100vh';
+	// document.body.style.width = '100vw';
+	// const tempHeightElement = document.createElement('div');
+	// tempHeightElement.style.height = '100vh';
+	// tempHeightElement.style.width = '100vw';
+	// tempHeightElement.style.position = 'absolute';
+	// tempHeightElement.style.top = '0';
+	// tempHeightElement.style.left = '0';
+	// tempHeightElement.style.visibility = 'hidden';
+	// tempHeightElement.style.pointerEvents = 'none';
+	// document.body.appendChild(tempHeightElement);
+	// const height = tempHeightElement.clientHeight;
+	// const width = tempHeightElement.clientWidth;
+	// document.body.removeChild(tempHeightElement);
 
 	// const height = window.innerHeight;
 	// const width = window.innerWidth;
 
-	// document.body.style.height = '100vh';
-	// document.body.style.width = '100vw';
-	// const height = document.body.clientHeight;
-	// const width = document.body.clientWidth;
+	document.body.style.height = '100vh';
+	document.body.style.width = '100vw';
+	const height = document.body.clientHeight;
+	const width = document.body.clientWidth;
 
 	return { height, width };
 };
