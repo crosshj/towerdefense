e97d33ae1582ae48b126c13732e2f26a43433a81
crosshj <github@chimpjuice.com>
2024-06-12 00:15:37 -0400

    Update getViewportDimensions.js

--- utils/getViewportDimensions.js
+++ utils/getViewportDimensions.js
@@ -14,34 +14,34 @@ const domLoaded = async () => {
 
 export const getViewportDimensions = async () => {
 	await domLoaded();
-	/*
-  // Create a temporary element to measure height
-  const tempHeightElement = document.createElement('div');
-  tempHeightElement.style.height = '100vh';
-  tempHeightElement.style.width = '100vw';
-  tempHeightElement.style.position = 'absolute';
-  tempHeightElement.style.top = '0';
-  tempHeightElement.style.left = '0';
-  tempHeightElement.style.visibility = 'hidden';
-  tempHeightElement.style.pointerEvents = 'none';
 
-  // Append it to the body
-  document.body.appendChild(tempHeightElement);
+	// Create a temporary element to measure height
+	const tempHeightElement = document.createElement('div');
+	tempHeightElement.style.height = '100vh';
+	tempHeightElement.style.width = '100vw';
+	tempHeightElement.style.position = 'absolute';
+	tempHeightElement.style.top = '0';
+	tempHeightElement.style.left = '0';
+	tempHeightElement.style.visibility = 'hidden';
+	tempHeightElement.style.pointerEvents = 'none';
 
-  // Get the dimensions in pixels
-  const height = tempHeightElement.clientHeight;
-  const width = tempHeightElement.clientWidth;
+	// Append it to the body
+	document.body.appendChild(tempHeightElement);
 
-  // Remove the temporary element
-  document.body.removeChild(tempHeightElement);
-  */
-	document.body.style.height = '100vh';
-	document.body.style.width = '100vw';
+	// Get the dimensions in pixels
+	const height = tempHeightElement.clientHeight;
+	const width = tempHeightElement.clientWidth;
+
+	// Remove the temporary element
+	document.body.removeChild(tempHeightElement);
 
 	// const height = window.innerHeight;
 	// const width = window.innerWidth;
-	const height = document.body.clientHeight;
-	const width = document.body.clientWidth;
+
+	// document.body.style.height = '100vh';
+	// document.body.style.width = '100vw';
+	// const height = document.body.clientHeight;
+	// const width = document.body.clientWidth;
 
 	return { height, width };
 };
