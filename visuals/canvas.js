c6e0d75d1709c705e5b9e1ca850076c3fa5a1683
crosshj <github@chimpjuice.com>
2024-06-12 23:17:44 -0400

    Update canvas.js

--- visuals/canvas.js
+++ visuals/canvas.js
@@ -22,3 +22,27 @@ export const canvasHorizontal = async (args) => {
 	}
 	return { canvas, ctx };
 };
+
+export const canvasVertical = async (args) => {
+	const { parent, image, width, scrollBottom } = args;
+	const canvas = document.createElement('canvas');
+	canvas.width = width;
+	canvas.height = parent.clientHeight;
+	const ctx = canvas.getContext('2d', {
+		antialias: false,
+		depth: false,
+		desynchronized: true
+	});
+	ctx.imageSmoothingEnabled = false;
+	parent.insertAdjacentElement('afterbegin', canvas);
+	if (image) {
+		await scrollable({
+			image,
+			vertical: true,
+			scrollBottom,
+			canvas,
+			ctx
+		});
+	}
+	return { canvas, ctx };
+};
