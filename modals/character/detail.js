1927e9e3f15dc3376f824195770f29e8e45f5bd2
crosshj <github@chimpjuice.com>
2024-07-16 01:58:01 -0400

    character detail WIP

--- modals/character/detail.js
+++ modals/character/detail.js
@@ -1,16 +1,68 @@
-document.addEventListener('DOMContentLoaded', async () => {
-	const params = Object.fromEntries(
-		new URLSearchParams(window.location.search)
+import { getTeamFromNumber } from '../../pages/_utils/getTeam.js';
+import { nodeTree, populateNodeTree } from './detailDom.js';
+
+const getElementColor = (element) => {
+	if (element === 'Fighting') return 'red';
+	if (element === 'Rock') return 'grey';
+	if (element === 'Fairy') return 'pink';
+	if (element === 'Air') return '#82cceb';
+	if (element === 'Dragon') return '#6320ff';
+	if (element === 'Fire') return 'orange';
+	if (element === 'Electric') return 'yellow';
+	if (element === 'Earth') return '#9c6943';
+	if (element === 'Dark') return '#6e62c8';
+	if (element === 'Bug') return '#a3b721';
+	const color = 'grey';
+	return color;
+};
+
+const updateValues = async ({ params, nodeTree }) => {
+	const { sub, slot, team = 1 } = params;
+	const currentTeam = await getTeamFromNumber(team);
+	const character = currentTeam[sub][Number(slot) - 1];
+
+	window.character = character;
+	window.nodeTree = nodeTree;
+
+	const { left, right } = nodeTree.container.content;
+	left.stars.innerText = '★'.repeat(character.stars);
+	left.character.image.append(character.image);
+	left.name.second.innerText = character.displayName;
+	left.element.name.innerText = character.element;
+	left.element.icon.style.background = getElementColor(character.element);
+	left.character.background.style.background = getElementColor(
+		character.element
 	);
-	console.log({ params });
+	left.loading.style.display = 'none';
 
-	// const rightPane = document.querySelector('.container .content > .right');
-	// rightPane.innerHTML = `WIP/TODO:<br/> load Team ${params.sub}, Slot ${params.slot}`;
+	right.rowOne.level.current.innerText = character.level;
+	const levelPercent =
+		Math.floor(Number(character.levelNextPercent) / 10) * 10;
+	right.rowOne.percent.number.innerText =
+		Math.floor(character.levelNextPercent) + '%';
+	right.rowOne.percent.bar.classList.remove('fill-0');
+	right.rowOne.percent.bar.classList.add('fill-' + levelPercent);
+	right.loading.style.display = 'none';
+	right.rowTwo.mineral.amount.innerText = character.mineralCost + '';
+	right.details.basic.metrics.attack.value.innerText = character.attack + '';
+	right.details.basic.metrics.health.value.innerText = character.hp + '';
+	right.details.basic.metrics.defense.value.innerText =
+		character.defense || 0 + '';
+};
 
-	const closeButton = document.querySelector('.close-button');
-	closeButton.addEventListener('pointerdown', () => {
+const attachHandlers = (nodeTree) => {
+	nodeTree.container.closeButton.addEventListener('pointerdown', () => {
 		window.parent.postMessage({
 			_: 'navigate'
 		});
 	});
+};
+
+document.addEventListener('DOMContentLoaded', async () => {
+	const params = Object.fromEntries(
+		new URLSearchParams(window.location.search)
+	);
+	populateNodeTree(nodeTree);
+	attachHandlers(nodeTree);
+	await updateValues({ nodeTree, params });
 });
---
+++ modals/character/detailDom.js
@@ -0,0 +1,115 @@
+export const nodeTree = {
+	background: undefined,
+	container: {
+		containerBg: undefined,
+		closeButton: undefined,
+		content: {
+			left: {
+				loading: undefined,
+				stars: undefined,
+				name: {
+					first: undefined,
+					second: undefined
+				},
+				element: {
+					icon: undefined,
+					name: undefined,
+					help: undefined
+				},
+				character: {
+					background: undefined,
+					image: undefined
+				},
+				gear: {
+					weapon: undefined,
+					armor: undefined,
+					accessory: undefined,
+					more: undefined
+				},
+				actions: {
+					levelUp: undefined,
+					potentialUp: undefined
+				}
+			},
+			right: {
+				loading: undefined,
+				rowOne: {
+					level: {
+						current: undefined,
+						slash: undefined,
+						max: undefined
+					},
+					cap: undefined,
+					percent: {
+						bar: undefined,
+						number: undefined
+					},
+					professorPoints: {
+						icon: undefined,
+						number: undefined
+					}
+				},
+				rowTwo: {
+					type: {
+						icon: undefined,
+						name: undefined,
+						help: undefined
+					},
+					mineral: {
+						icon: undefined,
+						amount: undefined
+					},
+					details: undefined
+				},
+				separator: undefined,
+				details: {
+					basic: {
+						metrics: {
+							attack: {
+								icon: undefined,
+								value: undefined
+							},
+							health: {
+								icon: undefined,
+								value: undefined
+							},
+							defense: {
+								icon: undefined,
+								value: undefined
+							}
+						},
+						speed: {
+							attackSpeed: undefined,
+							moveSpeed: undefined
+						},
+						skill: {
+							skillOne: undefined,
+							skillTwo: undefined,
+							skillThree: undefined
+						},
+						ability: {
+							abilityOne: undefined,
+							abilityTwo: undefined
+						}
+					},
+					advanced: undefined
+				},
+				actions: {
+					ultraEvolve: undefined,
+					hyperEvolve: undefined,
+					lock: undefined
+				}
+			}
+		}
+	}
+};
+
+export function populateNodeTree(obj, path = '') {
+	for (const key in obj) {
+		if (typeof obj[key] === 'object' && obj[key] !== null) {
+			populateNodeTree(obj[key], `${path} .${key}`);
+		} else {
+			obj[key] = document.querySelector(`${path} .${key}`);
+		}
+	}
+}
