14dcb148542416f25191aae36ab572ceeec96a43
crosshj <github@chimpjuice.com>
2024-08-10 00:52:28 -0400

    Update showNotifcation.js

--- serviceWorker/showNotifcation.js
+++ serviceWorker/showNotifcation.js
@@ -1,7 +1,7 @@
 export const showNotification = (data) => {
 	self.registration.showNotification(data.title, {
 		body: data.body,
-		badge: 'assets/towerDefenseIconTransparent.png',
+		badge: 'assets/favicon.png',
 		icon: 'assets/towerDefenseIconTransparent.png',
 		tag: data.tag || 'teedee', //will replace other notifications with this tag
 		data: { url: data.url || 'https://teedee.us' }
