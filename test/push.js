3d6c7e5e36e9c0dfe56a6c9e04423245f4a8fbe4
crosshj <github@chimpjuice.com>
2024-08-12 18:05:00 -0400

    push test + scale logo

--- test/push.js
+++ test/push.js
@@ -4,22 +4,85 @@ const setupPushNotifications = async (subscription) => {
 	// 	pushSubscription: subscription
 	// });
 
-	await fetch('https://datamosh.vercel.app/api/teedee/notification/send', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({
-			subscription,
-			payload: 'I ❤️ Aileen!'
-		})
+	return await sendNotification({ subscription, content: 'test message' });
+};
+
+async function sendNotification({ subscription, player, title, content }) {
+	let error;
+	const result = await fetch(
+		'https://datamosh.vercel.app/api/teedee/notification/send',
+		{
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				subscription,
+				player,
+				title,
+				payload: content
+			})
+		}
+	)
+		.then((x) => x.json())
+		.catch((e) => {
+			error = e;
+		});
+	if (error) return { error };
+	return result;
+}
+
+function getSelectedValue() {
+	const selectedRadio = document.querySelector('input[name="user"]:checked');
+	return selectedRadio ? selectedRadio.value : null;
+}
+
+const userRow = (user, i) => `
+	<label class="nameSelectContainer">
+		<input ${!i ? 'checked' : ''} value="${user.name}" type="radio" name="user"  />
+		<div class="userName">${user.name}</div>
+	</label>
+	<div></div>
+`;
+
+const attachUsersList = async () => {
+	const users = (
+		await fetch('https://datamosh.vercel.app/api/teedee/players').then(
+			(x) => x.json()
+		)
+	)
+		// .filter((x) => typeof x.last_login === 'string')
+		.sort((a, b) => a.name.localeCompare(b.name));
+
+	console.log(users);
+	const usersListEl = document.querySelector('.usersList');
+	usersListEl.innerHTML = users.map(userRow).join('\n');
+
+	const sendButton = document.querySelector('.sendButton');
+	const messageTitleEl = document.querySelector('#messageTitle');
+	const messageTextEl = document.querySelector('#messageText');
+
+	messageTitleEl.value = 'Test Title';
+	messageTextEl.value = 'Test Body';
+
+	sendButton.addEventListener('click', async () => {
+		//TODO: show loading
+		const selected = getSelectedValue();
+		const result = await sendNotification({
+			player: selected,
+			title: messageTitleEl.value,
+			content: messageTextEl.value
+		});
+		// todo hide loading
+		// update results element
+		console.log({ result });
 	});
 };
 
-const sub2 = {};
-
 const onLoaded = async () => {
-	await setupPushNotifications(sub2);
+	await attachUsersList();
+
+	//await setupPushNotifications(subscription);
 };
 
 document.addEventListener('DOMContentLoaded', onLoaded);
