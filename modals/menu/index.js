d3dc73096c3f2d52c845c87461f02be536c1c201
crosshj <github@chimpjuice.com>
2024-08-15 05:59:48 -0400

    init svg.icons, use in menu

--- modals/menu/index.js
+++ modals/menu/index.js
@@ -1,3 +1,5 @@
+import { SVGIcons } from '../../assets/icons.svg.js';
+
 const getNavigateTo = (el) => {
 	const permitted = [
 		'advent',
@@ -17,7 +19,46 @@ const getNavigateTo = (el) => {
 	return permitted.find((x) => el.classList.contains(x));
 };
 
+const icons = {
+	PVP: SVGIcons.swordCrossed,
+	'MAIN STAGE': SVGIcons.mainStage,
+	'MY TEAM': SVGIcons.stars6,
+	HOME: SVGIcons.returnHome,
+	'SPECIAL STAGE': SVGIcons.specialStage,
+	UPGRADE: SVGIcons.upgrade,
+	MISSION: SVGIcons.ticket,
+	ADVENT: SVGIcons.advent,
+	GEAR: SVGIcons.swordShield,
+	GACHA: SVGIcons.gacha,
+	TOWER: SVGIcons.tower,
+	GUILD: SVGIcons.guild,
+	LAB: SVGIcons.lab
+};
+
+const setupMenuGrid = () => {
+	const menuItems = Array.from(document.querySelectorAll('.menuGrid > div'));
+	for (const item of menuItems) {
+		const label = (item.innerText || '').trim();
+		if (!label) continue;
+		const icon = icons[label];
+		// if (!icon) continue;
+		item.innerHTML = `
+			<div class="menuItemContainer">
+				<div class="menuIcon">
+					${icon ? icon() : ''}
+				</div>
+				<div class="menuLabel">
+					${label}
+				</div>
+			</div>
+		`;
+	}
+};
+
 document.addEventListener('DOMContentLoaded', (event) => {
+	const container = document.querySelector('.container');
+	setupMenuGrid();
+	container.classList.remove('hidden');
 	document.body.addEventListener('pointerdown', (e) => {
 		const navigateTo = getNavigateTo(e.target);
 		const src = navigateTo ? `/pages/${navigateTo}/index.html` : undefined;
