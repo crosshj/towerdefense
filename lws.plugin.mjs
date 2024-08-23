7e180ee290ad40c68e452b81bee512e50281a09b
crosshj <github@chimpjuice.com>
2024-08-23 02:29:05 -0400

    misc

--- lws.plugin.mjs
+++ lws.plugin.mjs
@@ -1,22 +1,17 @@
 import * as fs from 'fs';
 import * as childProcess from 'child_process';
 
-let swCached;
 const sendModdedServiceWorker = (ctx) => {
 	ctx.set('Content-Type', 'application/javascript; charset=utf-8');
-	if (swCached) {
-		ctx.body = swCached;
-		return;
-	}
 	const serviceWorkerFileBody = fs.readFileSync('sw.js');
 	const revision = childProcess
 		.execSync('git rev-parse HEAD')
 		.toString()
 		.trim();
-	swCached = serviceWorkerFileBody
+	const modified = serviceWorkerFileBody
 		.toString()
 		.replace('{GIT_COMMIT_HASH}', revision);
-	ctx.body = swCached;
+	ctx.body = modified;
 	return;
 };
 
