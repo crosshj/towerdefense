6e7ea63c750ad87085b9eee5d92f499f7e1af9db
Aileen Cross <arcross28@gmail.com>
2024-08-28 17:47:40 -0400

    Update drops.js

--- $data/drops.js
+++ $data/drops.js
@@ -8,7 +8,7 @@ export const stageExperience = {
 	oasis1: () => ({ player: 50000, unit: 50000 }),
 	tundra1: () => ({ player: 70000, unit: 70000 }),
 	industrial1: () => ({ player: 85000, unit: 85000 }),
-	pantheon1: () => ({ player: 96000, unit: 96000 })
+	pantheon1: () => ({ player: 96000, unit: 96000 }),
 };
 
 export const stageCharacterDrops = {
@@ -21,7 +21,7 @@ export const stageCharacterDrops = {
 		'u0001-2-coco': { type: 'char', probability: 0.2 },
 		'u0001-4-aynstine': { type: 'char', probability: 0.2 },
 		'u0001-3-poki': { type: 'char', probability: 0.4 },
-		'u0001-4-sylvia': { type: 'char', probability: 0.4 }
+		'u0001-4-sylvia': { type: 'char', probability: 0.4 },
 	},
 	// grass, bug, normal
 	forest1: {
@@ -32,7 +32,7 @@ export const stageCharacterDrops = {
 		'u0001-3-pismud': { type: 'char', probability: 0.2 },
 		'u0001-4-openhyman': { type: 'char', probability: 0.2 },
 		'u0001-1-skye': { type: 'char', probability: 0.4 },
-		'u0001-4-ginger': { type: 'char', probability: 0.4 }
+		'u0001-4-ginger': { type: 'char', probability: 0.4 },
 	},
 	// coastal: water, normal, flying(air)
 	water1: {
@@ -42,47 +42,48 @@ export const stageCharacterDrops = {
 		'u0001-3-arcus': { type: 'char', probability: 0.3 },
 		'u0001-4-pineman': { type: 'char', probability: 0.4 },
 		'u0001-2-tom': { type: 'char', probability: 0.4 },
-		'u0001-4-lily': { type: 'char', probability: 0.4 }
+		'u0001-4-lily': { type: 'char', probability: 0.4 },
 	},
-	// grass, fighting, dragon
+	// grass, fighting, dragon, normal
 	sakura1: {
 		'u0001-1-macho': { type: 'char', probability: 0.1 },
-		'u0001-4-ruby': { type: 'char', probability: 0.2 },
-		'u0001-4-boar': { type: 'char', probability: 0.25 },
-		'u0001-5-popo': { type: 'char', probability: 0.45 }
+		'u0001-4-ruby': { type: 'char', probability: 0.1 },
+		'u0001-4-boar': { type: 'char', probability: 0.1 },
+		'u0001-5-popo': { type: 'char', probability: 0.2 },
+		'u0001-4-chizu': { type: 'char', probability: 0.5 },
 	},
 	// rock, ground, fighting
 	highlands1: {
 		'u0001-4-tabi': { type: 'char', probability: 0.1 },
 		'u0001-2-toto': { type: 'char', probability: 0.1 },
-		'u0001-4-liz': { type: 'char', probability: 0.4 }
+		'u0001-4-liz': { type: 'char', probability: 0.4 },
 	},
 	// tropical graveyard: ghost, dark, grass
 	tropical1: {
 		'u0001-5-multo': { type: 'char', probability: 0.1 },
 		'u0001-4-bumpier': { type: 'char', probability: 0.1 },
 		'u0001-2-ogre': { type: 'char', probability: 0.4 },
-		'u0001-3-corpus': { type: 'char', probability: 0.4 }
+		'u0001-3-corpus': { type: 'char', probability: 0.4 },
 	},
 	// thunder oasis: electric, ground, rock, fire
 	oasis1: {
 		'u0001-5-electopus': { type: 'char', probability: 0.1 },
 		'u0001-5-santelmo': { type: 'char', probability: 0.1 },
 		'u0001-1-benny': { type: 'char', probability: 0.3 },
-		'u0001-3-rose': { type: 'char', probability: 0.4 }
+		'u0001-3-rose': { type: 'char', probability: 0.4 },
 	},
 	// ice, water, ground
 	tundra1: {
 		'u0001-5-kelvin': { type: 'char', probability: 0.1 },
 		'u0001-4-slabb': { type: 'char', probability: 0.2 },
-		'u0001-4-violet': { type: 'char', probability: 0.4 }
+		'u0001-4-violet': { type: 'char', probability: 0.4 },
 	},
 	// steel, poison, electric
 	industrial1: {
 		'u0001-5-barbell': { type: 'char', probability: 0.1 },
 		'u0001-5-crack9': { type: 'char', probability: 0.1 },
 		'u0001-2-zerosix': { type: 'char', probability: 0.2 },
-		'u0001-2-jet': { type: 'char', probability: 0.4 }
+		'u0001-2-jet': { type: 'char', probability: 0.4 },
 	},
 	// dragon, fairy, psychic,
 	pantheon1: {
@@ -91,6 +92,6 @@ export const stageCharacterDrops = {
 		'u0001-3-twinkle': { type: 'char', probability: 0.1 },
 		'u0001-5-bibly': { type: 'char', probability: 0.3 },
 		'u0001-5-drat': { type: 'char', probability: 0.2 },
-		'u0001-4-crimson': { type: 'char', probability: 0.4 }
-	}
+		'u0001-4-crimson': { type: 'char', probability: 0.4 },
+	},
 };
