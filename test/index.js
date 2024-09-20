5019357b9ceb478e91d88f2877ec80b82200a92a
crosshj <github@chimpjuice.com>
2024-09-20 05:17:34 -0400

    stages/main background layers

--- test/index.js
+++ test/index.js
@@ -1,4 +1,6 @@
 const links = [
+	{ href: '/assets/stage/main/background.html', label: 'Main Map' },
+	{ href: '/assets/stage/candy/background.html', label: 'Candy Map' },
 	{ href: 'characters/index.html', label: 'Characters Animation Test' },
 	{ href: 'characters/sprites/index.html', label: 'Sprites Tool' },
 	{ href: 'characters/dragonbones/index.html', label: 'DragonBones Tool' },
