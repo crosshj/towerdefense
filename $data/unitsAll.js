4b290cfec0af0bb24791c55221c65dfeba3932d1
Yrill <106539458+Yrill@users.noreply.github.com>
2024-10-21 19:17:03 +0800

    new char

--- $data/unitsAll.js
+++ $data/unitsAll.js
@@ -1227,5 +1227,17 @@ export const unitsAll = {
 		range: 155,
 	},
 
+	'u0001-5-glacielle': {
+		displayName: 'Glacielle',
+		element: 'Mermaid',
+		type: 'Agility',
+		rank: 5,
+		mineralCost: 1000,
+		attack: '2,709 - 59,004 - 99,504',
+		hp: '44,872 - 902,502 - 1,519,502',
+		defense: '303 - 5,585 - 9,385',
+		range: 155,
+	},
+
 
 };
