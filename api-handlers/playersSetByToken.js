75010316f5fe5f783ff3e9f5372bab8c80e8c08f
crosshj <github@chimpjuice.com>
2025-02-06 11:16:59 -0500

    api: players setByToken

--- api-handlers/playersSetByToken.js
+++
@@ -1,9 +0,0 @@
-import { getCurrentVersion } from './version.js';
-
-export const playersSetByToken = ({ currentHash }) => ({
-	match: (url) => url.endsWith('/api/teedee/players/setByToken'),
-	async handler(url, options) {
-		const currentVersion = getCurrentVersion({ currentHash });
-		console.log('API: playersSetByToken');
-	},
-});
