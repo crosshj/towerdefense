c320ad41bfb0a93a6e593ed99fb7268ddcbd3767
crossar <41621244+crossar@users.noreply.github.com>
2025-02-07 14:23:08 -0500

    update character++

--- $data/gacha.js
+++ $data/gacha.js
@@ -75,6 +75,7 @@ const drops = {
 		{ code: 'u0001-4-pesto', probability: 1 },
 		{ code: 'u0001-4-pasta', probability: 1 },
 		{ code: 'u0001-4-cassis', probability: 1 },
+		{ code: 'u0001-4-pecan', probability: 1 },
 	],
 
 	'units-common': [
--- $data/unitsAll.js
+++ $data/unitsAll.js
@@ -1427,4 +1427,16 @@ export const unitsAll = {
 		defense: '303 - 5,585 - 9,385',
 		range: 200,
 	},
+
+	'u0001-4-pecan': {
+		displayName: 'Pecan',
+		element: 'Normal',
+		type: 'Agility',
+		rank: 4,
+		mineralCost: 150,
+		attack: '1,709 - 20,004 - 50,000',
+		hp: '4,872 - 92,502 - 150,000',
+		defense: '303 - 5,585 - 9,385',
+		range: 400,
+	},
 };
